a8fa827e697bbb881d307a5d23ac298802f03048
FISEI-LB1-PC18 <andrescarrera212@gmail.com>
2023-10-24 17:21:00 -0500

    Steven Carrera 17h15

---
+++ app/src/main/java/uta/fisei/steven_carrera_prueba01/activity_principal.java
@@ -0,0 +1,14 @@
+package uta.fisei.steven_carrera_prueba01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
